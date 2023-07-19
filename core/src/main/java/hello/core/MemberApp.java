3ab0eb34c6d47a14de6ba276c415792f85df1fc1
jun9898 <nice1998@gmail.com>
2023-07-20 01:00:12 +0900

    김영한 자바 스프링 핵심 원리 기본편 섹션 3 다 들었음 확실히 무료강의보다 이해도 잘되고 퀄리티가 엄청 높다

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -3,14 +3,16 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-//         AppConfig를 통해 memberService를 생성하여 의존성을 주입한다.
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+
+        // Spring bean에 들어있는 class를 다 가져옴
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(member);
 
