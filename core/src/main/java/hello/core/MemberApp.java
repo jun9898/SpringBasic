fa5073dade135f5c73e697bdc1d5e5b1127b6791
jun9898 <nice1998@gmail.com>
2023-07-19 19:04:43 +0900

    김영한 자바 스프링 핵심 원리 기본편 섹션 3 듣는중! OCP와 DIP에 대해 이해함

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -7,7 +7,10 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+//         AppConfig를 통해 memberService를 생성하여 의존성을 주입한다.
+        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(member);
 
