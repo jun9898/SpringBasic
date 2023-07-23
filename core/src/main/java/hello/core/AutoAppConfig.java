a84e2c569dc1147590821538b9397855cc6d104c
jun9898 <nice1998@gmail.com>
2023-07-23 15:16:26 +0900

    김영한 자바 스프링 강의 섹션 6 수강 완료 컴포넌트 스캔의 의존관계 자동 주입과 필터 설정방법 수강 완료

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+// 기존 예제코드를 살리기 위해 Configuration 필터를 적용시켜 제외해주었다.
+@ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    // 스프링에 중복된 bean이 등록될 시 Test
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
+}
