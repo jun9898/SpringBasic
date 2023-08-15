9ed62d322450761f04b616ab7224261ab602e34b
jun9898 <nice1998@gmail.com>
2023-08-15 17:57:43 +0900

    섹션 3 스프링 핵심 원리 이해2 객체 지향 원리 적용 수강 완료

--- 2회차/core/src/main/java/hello/core/OrderApp.java
+++ 2회차/core/src/main/java/hello/core/OrderApp.java
@@ -1,26 +1,29 @@
-package hello.core.member;
+package hello.core;
 
-import hello.core.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class MemberApp {
+public class OrderApp {
     public static void main(String[] args) {
-
-//        AppConfig appConfig = new AppConfig();
-//        MemberService memberService = new MemberServiceImpl();
 
         // 스프링 컨테이너에 AppConfig를 등록한다.
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
         // Bean의 이름을 조회하고 어떤 타입으로 return할건지 선언해준 뒤 memberService에 할당해준다.
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        Long memberId = 1L;
+        Member member = new Member(memberId,"memberA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1L);
-        System.out.println("new member = " + member);
-        System.out.println("find Member = " + findMember);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
+
+        System.out.println("order = " + order);
     }
 }
